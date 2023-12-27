f7761a3fbf423233415c234f4de28deb33b81aae
Igor Malrkov <130799861+igormalkov@users.noreply.github.com>
2023-12-27 20:32:27 +0100

    Create numberOfRowsInSection.swift

---
+++ numberOfRowsInSection.swift
@@ -0,0 +1,9 @@
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! CustomTableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
